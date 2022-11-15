6e32b79883eedb0848872d0ee53ebd51efe67fb7
leroysb <leroybuliro@gmail.com>
2022-11-14 23:04:38 -0800

    update structure

--- builtin.c
+++ builtin.c
@@ -1,105 +1,97 @@
 #include "shell.h"
 
 /**
- * num_builtins - holds the builtin strings.
- * Return: string size
+ * _myexit - exits the shell
+ * @info: Structure containing potential arguments. Used to maintain
+ *          constant function prototype.
+ *  Return: exits with a given exit status
+ *         (0) if info.argv[0] != "exit"
  */
-int num_builtins(void)
+int _myexit(info_t *info)
 {
-	char *builtin_str[] = {"cd", "exit"};
-	return (sizeof(builtin_str) / sizeof(char *));
+	int exitcheck;
+
+	if (info->argv[1])  /* If there is an exit arguement */
+	{
+		exitcheck = _erratoi(info->argv[1]);
+		if (exitcheck == -1)
+		{
+			info->status = 2;
+			print_error(info, "Illegal number: ");
+			_eputs(info->argv[1]);
+			_eputchar('\n');
+			return (1);
+		}
+		info->err_num = _erratoi(info->argv[1]);
+		return (-2);
+	}
+	info->err_num = -1;
+	return (-2);
 }
 
 /**
- * shell_cd - builtin command to change directory
- *
- * @args: argument list
- * Return: Always 1
+ * _mycd - changes the current directory of the process
+ * @info: Structure containing potential arguments. Used to maintain
+ *          constant function prototype.
+ *  Return: Always 0
  */
-int shell_cd(char **args)
+int _mycd(info_t *info)
 {
-	if (args[1] == NULL)
+	char *s, *dir, buffer[1024];
+	int chdir_ret;
+
+	s = getcwd(buffer, 1024);
+	if (!s)
+		_puts("TODO: >>getcwd failure emsg here<<\n");
+	if (!info->argv[1])
 	{
-		fprintf(stderr, "./shell: expected argument to \"cd\"\n");
+		dir = _getenv(info, "HOME=");
+		if (!dir)
+			chdir_ret = /* TODO: what should this be? */
+				chdir((dir = _getenv(info, "PWD=")) ? dir : "/");
+		else
+			chdir_ret = chdir(dir);
+	}
+	else if (_strcmp(info->argv[1], "-") == 0)
+	{
+		if (!_getenv(info, "OLDPWD="))
+		{
+			_puts(s);
+			_putchar('\n');
+			return (1);
+		}
+		_puts(_getenv(info, "OLDPWD=")), _putchar('\n');
+		chdir_ret = /* TODO: what should this be? */
+			chdir((dir = _getenv(info, "OLDPWD=")) ? dir : "/");
+	}
+	else
+		chdir_ret = chdir(info->argv[1]);
+	if (chdir_ret == -1)
+	{
+		print_error(info, "can't cd to ");
+		_eputs(info->argv[1]), _eputchar('\n');
 	}
 	else
 	{
-		if (chdir(args[1]) != 0)
-		{
-			perror("./shell");
-		}
+		_setenv(info, "OLDPWD", _getenv(info, "PWD="));
+		_setenv(info, "PWD", getcwd(buffer, 1024));
 	}
-	return (1);
-}
-
-/**
- * shell_exit - runs builtin exit command
- *
- * @args: argument list
- * Return: Always 0
- */
-int shell_exit(char **args)
-{
-	args = args;
 	return (0);
 }
 
 /**
- * launch - start a program
- *
- * args: null terminated list of arguments
- * Return: Always 1
+ * _myhelp - changes the current directory of the process
+ * @info: Structure containing potential arguments. Used to maintain
+ *          constant function prototype.
+ *  Return: Always 0
  */
-int launch(char **)
+int _myhelp(info_t *info)
 {
-	pid_t pid;
-	int status;
-	pid = fork();
+	char **arg_array;
 
-	if (pid == 0)
-	{
-		if (execve(args[0], args) == -1)
-		{
-			perror("./shell");
-		}
-		exit(EXIT_FAILURE);
-	}
-	else if (pid < 0)
-	{
-		perror("./shell");
-	}
-	else
-	{
-		do {
-			waitpid(pid, &status, WUNTRACED);
-		} while (!WIFEXITED(status) && !WIFSIGNALED(status));
-	}
-	return (1);
+	arg_array = info->argv;
+	_puts("help call works. Function not yet implemented \n");
+	if (0)
+		_puts(*arg_array); /* temp att_unused workaround */
+	return (0);
 }
-
-/**
- * execute - executes builtins
- *
- * @args: null terminated list of arguments
- * Return: 1 if shell continues and 0 if to terminate
- */
-int execute(char **args)
-{
-	char *builtin_str[] = {"cd", "exit"};
-	int (*builtin_func[])(char **) = {&shell_cd, &shell_exit};
-	int i;
-
-	if (args[0] == NULL)
-	{
-		return (1);
-	}
-	for (i = 0; i < num_builtins(); i++)
-	{
-		if (strcmp(args[0], builtin_str[i]) == 0)
-		{
-			return ((*builtin_func[i])(args));
-		}
-	}
-	return (launch(args));
-}
-	
